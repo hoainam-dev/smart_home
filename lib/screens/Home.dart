add2cbda0daa452ff3bc7a9d3cd4ddf2c6ad6b9b
hoainam-dev <nam54529@donga.edu.vn>
2023-08-23 12:50:14 +0700

    update code

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -3,6 +3,7 @@ import 'package:card_swiper/card_swiper.dart';
 
 class Light {
   bool isOn;
+
   Light({required this.isOn});
 }
 
@@ -10,6 +11,7 @@ class Items {
   Icon iconOn;
   Icon iconOff;
   String name;
+
   Items({required this.iconOn, required this.iconOff, required this.name});
 }
 
@@ -21,7 +23,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List<Light> lights = [
     Light(isOn: false),
     Light(isOn: false),
@@ -30,31 +31,71 @@ class _HomeState extends State<Home> {
   ];
 
   List<BoxWidget> boxWidgets = const [
-    BoxWidget(title: "LIVING ROOM",temp: "26C", humid: "50%", img: "assets/images/living_room.jpg"),
-    BoxWidget(title: "BED ROOM",temp: "22C", humid: "30%", img: "assets/images/bed_room.jpg"),
-    BoxWidget(title: "KITCHEN ROOM",temp: "22C", humid: "40%", img: "assets/images/kitchen_room.jpg")
+    BoxWidget(
+        title: "LIVING ROOM",
+        temp: "26\u2103",
+        humid: "50%",
+        img: "assets/images/living_room.jpg"),
+    BoxWidget(
+        title: "BED ROOM",
+        temp: "22\u2103",
+        humid: "30%",
+        img: "assets/images/bed_room.jpg"),
+    BoxWidget(
+        title: "KITCHEN ROOM",
+        temp: "22\u2103",
+        humid: "40%",
+        img: "assets/images/kitchen_room.jpg")
   ];
 
   List<Items> items = [
-    Items(iconOn:const Icon(Icons.lightbulb,size: 50,
-      color: Colors.white,),
-        iconOff:const Icon(Icons.lightbulb_outline,size: 50,
-      color: Colors.white,),
+    Items(
+        iconOn: const Icon(
+          Icons.lightbulb,
+          size: 50,
+          color: Colors.white,
+        ),
+        iconOff: const Icon(
+          Icons.lightbulb_outline,
+          size: 50,
+          color: Colors.white,
+        ),
         name: "ĐÈN"),
-    Items(iconOn:const Icon(Icons.camera_indoor,size: 50,
-      color: Colors.white,),
-        iconOff:const Icon(Icons.camera_indoor_outlined,size: 50,
-          color: Colors.white,),
+    Items(
+        iconOn: const Icon(
+          Icons.camera_indoor,
+          size: 50,
+          color: Colors.white,
+        ),
+        iconOff: const Icon(
+          Icons.camera_indoor_outlined,
+          size: 50,
+          color: Colors.white,
+        ),
         name: "CAMERA"),
-    Items(iconOn:const Icon(Icons.ac_unit,size: 50,
-      color: Colors.white,),
-        iconOff:const Icon(Icons.ac_unit_outlined,size: 50,
-          color: Colors.white,),
+    Items(
+        iconOn: const Icon(
+          Icons.ac_unit,
+          size: 50,
+          color: Colors.white,
+        ),
+        iconOff: const Icon(
+          Icons.ac_unit_outlined,
+          size: 50,
+          color: Colors.white,
+        ),
         name: "QUẠT"),
-    Items(iconOn:const Icon(Icons.chalet,size: 50,
-      color: Colors.white,),
-        iconOff:const Icon(Icons.chalet_outlined,size: 50,
-          color: Colors.white,),
+    Items(
+        iconOn: const Icon(
+          Icons.chalet,
+          size: 50,
+          color: Colors.white,
+        ),
+        iconOff: const Icon(
+          Icons.chalet_outlined,
+          size: 50,
+          color: Colors.white,
+        ),
         name: "LÒ SƯỞI"),
   ];
 
@@ -68,6 +109,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green.shade800,
       ),
@@ -81,130 +123,187 @@ class _HomeState extends State<Home> {
             ),
             child: Stack(
               children: [
-                SizedBox(
-                  height: 300,
+                Container(
+                  margin: EdgeInsets.fromLTRB(10, 100, 10, 0),
+                  height: 630,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.green.shade300,
+                          Colors.green.shade500,
+                          Colors.green.shade700,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.7),
+                          spreadRadius: 5,
+                          blurRadius: 15,
+                          offset: const Offset(0, 9),
+                        ),
+                      ]),
+                ),
+                Container( //Light Widget
+                  margin: const EdgeInsets.fromLTRB(30, 320, 30, 0),
+                  padding: const EdgeInsets.all(10),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 40.0,
+                      crossAxisSpacing: 40.0,
+                    ),
+                    itemCount: lights.length,
+                    itemBuilder: (context, index) {
+                      return LightWidget(
+                        isOn: lights[index].isOn,
+                        onTap: () => toggleLight(index),
+                        iconOn: items[index].iconOn,
+                        iconOff: items[index].iconOff,
+                        name: items[index].name,
+                      );
+                    },
+                  ),
+                ),
+                Container( //Slide Widget
+                  margin: EdgeInsets.fromLTRB(30, 120, 30, 20),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.lightGreen.shade200.withOpacity(0.6),
+                        Colors.lightGreen.shade300.withOpacity(0.7),
+                        Colors.lightGreen.shade500.withOpacity(0.8),
+                        Colors.lightGreen.shade700.withOpacity(0.9),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        offset: Offset(0, 15),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  height: 200,
                   child: Swiper(
-                    itemBuilder: (BuildContext context,int index){
+                    itemBuilder: (BuildContext context, int index) {
                       return boxWidgets[index];
                     },
                     itemCount: 3,
                     pagination: const SwiperPagination(),
-                    control: const SwiperControl(),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  child: Center(
-                    child: Column(
-                      children: const [
-                        Text("SMART HOME", style: TextStyle(fontSize: 50,
-                            color: Colors.orange, fontWeight: FontWeight.bold))
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 250, 0, 0),
-                  padding: const EdgeInsets.all(8),
-                  child: Center(
-                    child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 4.0,
-                        crossAxisSpacing: 8.0,
+                  margin: const EdgeInsets.fromLTRB(40, 30, 0, 0),
+                  child: ShaderMask(
+                    shaderCallback: (Rect bounds) {
+                      final gradient = LinearGradient(
+                        colors: [Colors.red, Colors.orange],
+                        stops: [0.0, 1.0],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      );
+
+                      return gradient.createShader(
+                        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                      );
+                    },
+                    child: const Text(
+                      'SMART HOME',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        color: Colors
+                            .white, // Ensure the text color matches the shader
                       ),
-                      itemCount: lights.length,
-                      itemBuilder: (context, index) {
-                        return
-                          Padding(
-                              padding: const EdgeInsets.fromLTRB(30,0,30,0), // Adjust padding here
-                              child: LightWidget(
-                                isOn: lights[index].isOn,
-                                onTap: () => toggleLight(index),
-                                iconOn: items[index].iconOn,
-                                iconOff: items[index].iconOff,
-                                name: items[index].name,
-                              )
-                          );
-                      },
                     ),
                   ),
-                )
+                ),
               ],
-            )
-        ),
+            )),
       ),
     );
   }
 }
 
-class BoxWidget extends StatelessWidget{
+class BoxWidget extends StatelessWidget {
   final String title, temp, humid, img;
 
-  const BoxWidget({
-    required this.title,
-    required this.temp,
-    required this.humid,
-    required this.img
-  });
+  const BoxWidget(
+      {required this.title,
+      required this.temp,
+      required this.humid,
+      required this.img});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.fromLTRB(50, 100, 50, 0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.green.shade200.withOpacity(0.7),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.8),
-              spreadRadius: 2,
-              blurRadius: 10,
-              offset: const Offset(0,5),
-            ),
-          ],
-        ),
         child: Stack(
-          children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(img, height: 100, width: 100,fit: BoxFit.cover,),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    width: 120,
-                    child: Text(title, style: const TextStyle(fontSize: 25,), maxLines:2,textAlign: TextAlign.center,),
-                  ),
-                  const Icon(Icons.info_outline)
-                ],
+      children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                img,
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-              padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Temperature", style: TextStyle(fontSize: 18),),
-                  Text(temp, style: const TextStyle(fontSize: 18))
-                ],),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 150, 0, 0),
-              padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Humidity", style: TextStyle(fontSize: 18),),
-                  Text(humid, style: const TextStyle(fontSize: 18))
-                ],),
-            ),
-          ],
-        )
-    );
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                width: 120,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 25,
+                  ),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Icon(Icons.info_outline)
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Temperature",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(temp, style: const TextStyle(fontSize: 18))
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 150, 0, 0),
+          padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Humidity",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(humid, style: const TextStyle(fontSize: 18))
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
 
@@ -229,28 +328,52 @@ class LightWidget extends StatelessWidget {
       onTap: onTap,
       child: Container(
           decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: isOn ? Colors.orange.withOpacity(0.7) : Colors.green.shade200.withOpacity(0.7),
+            gradient: isOn
+                ? LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.orange.shade200.withOpacity(0.6),
+                      Colors.orange.shade300.withOpacity(0.7),
+                      Colors.orange.shade500.withOpacity(0.8),
+                      Colors.orange.shade700.withOpacity(0.9),
+                    ],
+                  )
+                : LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.lightGreen.shade200.withOpacity(0.6),
+                      Colors.lightGreen.shade300.withOpacity(0.7),
+                      Colors.lightGreen.shade500.withOpacity(0.8),
+                      Colors.lightGreen.shade700.withOpacity(0.9),
+                    ],
+                  ),
+            borderRadius: BorderRadius.circular(30),
             boxShadow: [
               BoxShadow(
                 color: isOn
                     ? Colors.orange.withOpacity(1)
                     : Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 50,
-                offset: const Offset(0,3),
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: const Offset(5, 5),
               ),
             ],
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(name, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+              Text(
+                name,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
               isOn ? iconOn : iconOff,
+              SizedBox(height: 10),
               Text(isOn ? 'ON' : 'OFF'),
             ],
-          )
-      ),
+          )),
     );
   }
 }
